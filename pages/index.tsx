5d72bbfc293ed4236ab8fd12835b411cabf2e640
Timimah <44163985+Timimah@users.noreply.github.com>
2023-08-07 14:09:09 +0100

    chore: add semicolon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import Image from "next/image";
 import HeroSection from "@/components/organisms/HeroSection";
-import TeamSection from "@/components/organisms/TeamSection"
+import TeamSection from "@/components/organisms/TeamSection";
 import Layout from "@/components/layouts";
 
 export default function Home() {
   return (
     <Layout>
       <main>
-      <HeroSection />
-      <TeamSection />
-    </main>
+        <HeroSection />
+        <TeamSection />
+      </main>
     </Layout>
   );
 }
