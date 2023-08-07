0025b702db748c62fbc7a6ccab5b83140fefea15
Timimah <44163985+Timimah@users.noreply.github.com>
2023-08-07 13:51:39 +0100

    fix: delete 'develop'

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,14 @@ import Image from "next/image";
 import HeroSection from "@/components/organisms/HeroSection";
 import TeamSection from "@/components/organisms/TeamSection"
 import Layout from "@/components/layouts";
-develop
 
 export default function Home() {
   return (
     <Layout>
+      <main>
       <HeroSection />
       <TeamSection />
     </main>
     </Layout>
-develop
   );
 }
