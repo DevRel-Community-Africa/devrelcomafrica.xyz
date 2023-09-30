3b521849de8b4ff9db239d0712380fe4c5b98bdc
hacktivist123 <akintayoshedrack@gmail.com>
2023-09-30 02:41:02 +0100

    feat: add team section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import HeroSection from "@/components/organisms/HeroSection";
+import { StorySection } from "@/components/organisms/StorySection/StorySection";
+import { TeamSection } from "@/components/organisms/TeamSection/TeamSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <StorySection />
+      <TeamSection />
     </main>
   );
 }
