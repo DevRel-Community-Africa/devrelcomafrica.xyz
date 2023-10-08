94ee349d9e7d5b911b06740e138050ae8fbc9edf
Jamin <okpusjamin@gmail.com>
2023-10-08 14:23:37 +0100

    feat: update layout and responsiveness of story sction

--- components/organisms/StorySection/StorySection.tsx
+++ components/organisms/StorySection/StorySection.tsx
@@ -1,14 +1,16 @@
+import Image from "next/image";
+import blob from "@/public/blob.png";
+
 export function StorySection() {
   return (
-    <section className="bg-secondary p-20">
+    <section className="bg-secondary px-8 py-20 relative">
       <div className="container mx-auto px-2 py-2">
         <div className="text-neutral-800 text-[32px] font-semibold">
           The story
         </div>
-        <hr className="w-[156px] h-px bg-primary" />
+        <hr className="w-[156px] h-0.5 bg-primary" />
         <div>
-          <br></br>
-          <p className="w-[1050px] h-[250px] top-0 left-0 font-normal text-[#222222] text-[24px] leading-[normal] pt-2">
+          <p className="mt-10 font-normal text-[#222222] text-[24px] leading-[normal] pt-2 z-10 relative">
             <span className="font-light">
               Five years ago when I was starting out my tech journey in Nigeria,
               there was no structured way of learning how to code or design,
@@ -31,6 +33,12 @@ export function StorySection() {
           </p>
         </div>
       </div>
+      <Image
+        src={blob}
+        className="absolute right-0 bottom-2"
+        alt="BG-pattern"
+        width={400}
+      />
     </section>
   );
 }
