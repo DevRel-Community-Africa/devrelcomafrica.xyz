8dae7a2986fa9c9d67e2de990b9a5c5a34822dc8
Shedrack akintayo <akintayoshedrack@gmail.com>
2023-10-19 10:14:43 +0100

    fix failing build

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import HeroSection from "@/components/organisms/HeroSection";
 import Layout from "@/components/layouts";
 import { StorySection } from "@/components/organisms/StorySection/StorySection";
 import { TeamSection } from "@/components/organisms/TeamSection/TeamSection";
-import { MissionSection } from "@/components/organisms/MissionSection";
+import MissionSection from "@/components/organisms/MissionSection";
 
 
 export default function Home() {
