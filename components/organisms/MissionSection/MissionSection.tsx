f927a964c2b05f8c96dbc4af22db5e4fcadbc3a6
Desmond Obisi <51109125+DesmondSanctity@users.noreply.github.com>
2023-10-19 05:04:34 -0400

    Feat: Add Mission Section to Webpage (#13)
    * feat: develop mission section with responsiveness
    
    * refactor: update section to fit and be responsive
    
    ---------
    
    Co-authored-by: Jamin <okpusjamin@gmail.com>

---
+++ components/organisms/MissionSection/MissionSection.tsx
@@ -0,0 +1,87 @@
+import Image from "next/image";
+import blob from "@/public/blob-aesthetic.png";
+
+export function MissionSection() {
+  const missions = [
+    {
+      id: 1,
+      title:
+        "Educate more developers and technical writers in Africa about the significance of developer advocacy and how to pursue a career in this field.",
+      titleS:
+        "Educate about the significance of developer advocacy and how to pursue a career in this field.",
+    },
+    {
+      id: 2,
+      title:
+        "Organise free workshops and meet-ups to teach important concepts in Developer Relations and Advocacy with guest speakers from top companies around the world.",
+      titleS:
+        "Organise workshops and meet-ups to teach concepts in Developer Relations and Advocacy.",
+    },
+    {
+      id: 3,
+      title:
+        "Share DevRel job postings, review CVs and help the community members get ready for interviews.",
+      titleS:
+        "Share DevRel job postings, review CVs and help the community members get ready for interviews.",
+    },
+    {
+      id: 4,
+      title:
+        "Collaborate with other existing DevRel communities around the world to share knowledge.",
+      titleS:
+        "Collaborate with other existing DevRel communities around the world to share knowledge.",
+    },
+    {
+      id: 5,
+      title:
+        "Encourage African companies to implement a Developer Relations strategy.",
+      titleS:
+        "Encourage African companies to implement a Developer Relations strategy.",
+    },
+    {
+      id: 6,
+      title:
+        "Get price discounts on DevRel conferences and courses for community members.",
+      titleS:
+        "Get price discounts on DevRel conferences and courses for community members.",
+    },
+  ];
+
+  return (
+    <section className="bg-green-100 px-8 py-20 relative">
+      <div className="container mx-auto px-2 py-2">
+        <div className="text-neutral-800 text-[32px] font-semibold">
+          The Mission
+        </div>
+        <div className="mt-6 sm:mt-6 md:mt-8 lg:mt-10 text-[#222] lg:text-2xl text-xl ">
+          Developer Relations is a significant field in the tech industry.
+          Despite the high demand for developer relations professionals during
+          this time, there has been little adoption and demand for this field in
+          Africa, with only a small number of professionals from the continent.
+          DevRel Community Africa is a non-profit community that aims to bring
+          together developer relations experts and enthusiasts from all parts of
+          Africa to share knowledge, support each other&apos;s growth and
+          promote the field of developer relations in the region.
+        </div>
+      </div>
+      <div className="container mx-auto px-2 py-2">
+        <div className=" mt-6 grid md:grid-cols-2 lg:grid-cols-3 gap-6 z-10 relative">
+          {missions &&
+            missions.map((mission) => (
+              <div className="bg-white px-5 pb-5 pt-10" key={mission.id}>
+                <hr className=" bg-[#222] h-1 w-[50px]" />
+                <div className="w-85 left-0  text-emerald-700  mt-4 md:text-xl text-normal">
+                  {mission.title}
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+      <Image
+        src={blob}
+        alt="Mission bg pattern"
+        className="absolute left-0 -bottom-20"
+      />
+    </section>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import HeroSection from "@/components/organisms/HeroSection";
 import Layout from "@/components/layouts";
+import MissionSection from "@/components/organisms/MissionSection";
 
 export default function Home() {
   return (
     <Layout>
       <HeroSection />
+      <MissionSection />
     </Layout>
   );
 }
